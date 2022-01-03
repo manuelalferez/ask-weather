9e7e3975519a2473199a8b6e9dbf1710c0fe40ed
Manuel Alférez <manuelalferezruiz@gmail.com>
2022-01-03 10:18:28 +0100

    Adding config for Apollo and GraphQL: fetching the data

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import { gql } from "@apollo/client";
+import client from "../config/apollo-client";
 
-export default function Home() {
+export default function Home({ weather }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -12,20 +14,55 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a
-            className="text-blue-600"
-            href="https://github.com/manuelalferez/ask-weather"
-          >
-            Ask Weather!
-          </a>
-        </h1>
+        {JSON.stringify(weather, null, "\t")}
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        Footer
-      </footer>
+      <footer className="flex items-center justify-center w-full h-24 border-t"></footer>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query {
+        getCityByName(name: "Basel") {
+          id
+          name
+          country
+          coord {
+            lon
+            lat
+          }
+          weather {
+            summary {
+              title
+              description
+              icon
+            }
+            temperature {
+              actual
+              min
+              max
+            }
+            wind {
+              speed
+            }
+            clouds {
+              all
+              visibility
+              humidity
+            }
+            timestamp
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      weather: data.getCityByName,
+    },
+  };
 }
